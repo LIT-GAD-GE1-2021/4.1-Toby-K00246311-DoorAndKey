d39ec7a0a607837e3dbbdddbd7df3b89a6fce11b
Toby.Taylor <toby.taylor20@yahoo.com>
2021-02-04 10:46:42 +0000

    Implementing Stuff n' things

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    public GameObject theCharacter;
+    public GameObject theKey;
+    public GameObject keyHole;
+    public GameObject theDoor;
+
+    private bool hasKey;
+
+    private void Awake()
+    {
+        // set the instance property/variable to this object
+        instance = this;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+    }
+    
+}
